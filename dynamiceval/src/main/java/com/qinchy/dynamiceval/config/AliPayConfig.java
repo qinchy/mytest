7e6e5da3fcb45aabd67a711de2ed26af1f4de701
秦朝勇 <qinchy@msn.com>
2023-04-17 10:25:17 +0800

    配置化及SpEL

---
+++ dynamiceval/src/main/java/com/qinchy/dynamiceval/config/AliPayConfig.java
@@ -0,0 +1,20 @@
+package com.qinchy.dynamiceval.config;
+
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@ConfigurationProperties(prefix = "alipay.config")
+public class AliPayConfig {
+ 
+    // 商户appid
+    private String appId;
+ 
+    public String getAppId() {
+        return appId;
+    }
+ 
+    public void setAppId(String appId) {
+        this.appId = appId;
+    }
+}
