8983f0b59fde348dcf10bc4ff9279ca522692381
秦朝勇 <qinchy@msn.com>
2023-04-24 11:30:23 +0800

    配置化及SpEL

--- dynamiceval/src/main/java/com/qinchy/dynamiceval/controller/DynamicEvalController.java
+++ dynamiceval/src/main/java/com/qinchy/dynamiceval/controller/DynamicEvalController.java
@@ -5,7 +5,7 @@ import com.alibaba.fastjson.JSONArray;
 import com.alibaba.fastjson.JSONObject;
 import com.jayway.jsonpath.DocumentContext;
 import com.jayway.jsonpath.JsonPath;
-import com.qinchy.dynamiceval.config.AliPayConfig;
+import com.qinchy.dynamiceval.annotation.OperaLog;
 import com.qinchy.dynamiceval.service.AliPayConfigService;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -19,6 +19,7 @@ import org.springframework.expression.spel.standard.SpelExpressionParser;
 import org.springframework.expression.spel.support.StandardEvaluationContext;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.Map;
@@ -56,6 +57,8 @@ public class DynamicEvalController {
 
         EvaluationContext evaluationContext = new StandardEvaluationContext();
         evaluationContext.setVariable("aliPayConfigService", aliPayConfigService);
+        Boolean newStringParam = true;
+        evaluationContext.setVariable("newStringParam", newStringParam);
 
         //创建ExpressionParser解析表达式
         ExpressionParser parser = new SpelExpressionParser();
@@ -69,9 +72,12 @@ public class DynamicEvalController {
             try {
                 exp = parser.parseExpression(valueOrExpress);
                 value = exp.getValue();
+                if (null == value) {
+                    value = exp.getValue(evaluationContext, String.class);
+                }
             } catch (ParseException e) {
                 log.error("解析SpEL表达式{}异常，值可能是字面文本值", valueOrExpress, e);
-            } catch (SpelEvaluationException e){
+            } catch (SpelEvaluationException e) {
                 log.error("取值SpEL表达式{}异常，值可能是字面文本值", valueOrExpress, e);
             }
 
@@ -98,4 +104,19 @@ public class DynamicEvalController {
 
         return JSON.parseObject(documentContext.jsonString(), Map.class);
     }
+
+    /**
+     * 验证把参数放到注解里面，然后结合aspect实现日志中获取注解的值
+     *
+     * @param ip       参数传入ip
+     * @param userId   参数传入userid
+     * @param userName 参数传入userName
+     * @return
+     */
+    @PostMapping("/delete")
+    @OperaLog(message = "删除人员", ip = "#ip")
+    public String delete(@RequestParam("ip") String ip, @RequestParam("userId") String userId, @RequestParam("userName") String userName) {
+        return "DeleteSuccess";
+    }
+
 }
