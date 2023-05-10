ab3bad87d3eb0f34ce828a11cea241965ec8cc4a
qinchy <qinchy@msn.com>
2023-05-10 18:51:45 +0800

    增加签名和验签demo

--- jce-demo/src/main/java/com/qinchy/jcedemo/cipherdemo/CipherAsymmetricEncryptionDemo.java
+++ jce-demo/src/main/java/com/qinchy/jcedemo/cipherdemo/CipherAsymmetricEncryptionDemo.java
@@ -23,6 +23,8 @@ import java.security.spec.X509EncodedKeySpec;
 
 /**
  * 使用Cipher实现非对称加密（asymmetric encryption）示例
+ *
+ * @author qinchy
  */
 public class CipherAsymmetricEncryptionDemo {
 
@@ -47,7 +49,7 @@ public class CipherAsymmetricEncryptionDemo {
      *
      * @param secureRandomSeed ：SecureRandom 随机数生成器的种子，只要种子相同，则生成的公钥、私钥就是同一对.
      *                         <p>randomSeed 长度可以自定义，加/解密必须是同一个.</p>
-     * @return
+     * @return 生成 RSA 密钥对
      */
     public static KeyPair generateRsaKeyPair(String secureRandomSeed) {
         //KeyPair 是密钥对（公钥和私钥）的简单持有者。加密、解密都需要使用.
@@ -59,10 +61,10 @@ public class CipherAsymmetricEncryptionDemo {
             SecureRandom secureRandom = SecureRandom.getInstance("SHA1PRNG");
             //重新设定此随机对象的种子.
             secureRandom.setSeed(secureRandomSeed.getBytes());
-            /**
-             * initialize(int keySize, SecureRandom random):使用给定的随机源（random）初始化特定密钥大小的密钥对生成器。
-             * keySize: 健的大小值，这是一个特定于算法的度量。值越大，能加密的内容就越多，否则会抛异常：javax.crypto.IllegalBlockSizeException: Data must not be longer than xxx bytes
-             * 如 keySize 为 2048 时加密数据的长度不能超过 245 字节。
+            /*
+              initialize(int keySize, SecureRandom random):使用给定的随机源（random）初始化特定密钥大小的密钥对生成器。
+              keySize: 健的大小值，这是一个特定于算法的度量。值越大，能加密的内容就越多，否则会抛异常：javax.crypto.IllegalBlockSizeException: Data must not be longer than xxx bytes
+              如 keySize 为 2048 时加密数据的长度不能超过 245 字节。
              */
             keyPairGenerator.initialize(KEY_PAIR_LENGTH, secureRandom);
             //genKeyPair(): 生成密钥对
@@ -87,21 +89,28 @@ public class CipherAsymmetricEncryptionDemo {
         try {
             //获取 RSA 密钥对
             KeyPair keyPair = generateRsaKeyPair(secureRandomSeed);
-            /**getPrivate()：获取密钥对的私钥。
-             * getEncoded(): 返回已编码的密钥，如果密钥不支持编码，则返回 null*/
+
+            // getPrivate()：获取密钥对的私钥。
+            // getEncoded(): 返回已编码的密钥，如果密钥不支持编码，则返回 null
             byte[] privateEncoded = keyPair.getPrivate().getEncoded();
-            /**PKCS8EncodedKeySpec(byte[] encodedKey): 使用给定的编码密钥创建新的 PKCS8EncodedKeySpec
-             * PKCS8EncodedKeySpec 表示 ASN.1 号私钥的编码*/
+
+            // PKCS8EncodedKeySpec(byte[] encodedKey): 使用给定的编码密钥创建新的 PKCS8EncodedKeySpec
+            // PKCS8EncodedKeySpec:表示私有密钥的ASN.1编码
+            // Only RSAPrivate(Crt)KeySpec and PKCS8EncodedKeySpec supported for RSA private keys
             EncodedKeySpec encodedKeySpec = new PKCS8EncodedKeySpec(privateEncoded);
+
             //创建 KeyFactory 对象，用于转换指定算法(RSA)的公钥/私钥。
             KeyFactory keyFactory = KeyFactory.getInstance("RSA");
+
             //从提供的密钥规范生成私钥对象
             PrivateKey keyPrivate = keyFactory.generatePrivate(encodedKeySpec);
+
             //实例化 Cipher 对象。
             result = encryptionDecryption(content, model, keyPrivate);
         } catch (Exception e) {
             e.printStackTrace();
         }
+
         return result;
     }
 
@@ -119,6 +128,7 @@ public class CipherAsymmetricEncryptionDemo {
         try {
             // 得到公钥
             KeyPair keyPair = generateRsaKeyPair(secureRandomSeed);
+            // Only RSAPublicKeySpec and X509EncodedKeySpec supported for RSA public keys
             EncodedKeySpec keySpec = new X509EncodedKeySpec(keyPair.getPublic().getEncoded());
             KeyFactory keyFactory = KeyFactory.getInstance("RSA");
             PublicKey keyPublic = keyFactory.generatePublic(keySpec);
@@ -136,29 +146,34 @@ public class CipherAsymmetricEncryptionDemo {
      * @param content :待加解密的数据
      * @param model   ：1 表示加密模式（Cipher.ENCRYPT_MODE），2 表示解密模式（Cipher.DECRYPT_MODE）
      * @param key     ：公钥（PUBLIC_KEY）或 私钥（PRIVATE_KEY）的 key
-     * @return
+     * @return 加密或者解密结果字符串
      */
     private static String encryptionDecryption(String content, int model, Key key) {
         String result = "";
         try {
             Cipher cipher = Cipher.getInstance(CIPHER_TRANSFORMS);
+
             //init(int opMode, Key key)：初始化 Cipher.
             cipher.init(model, key);
+
             //如果是解密模式，则需要使用 Base64 进行解码.
             byte[] contentBytes = content.getBytes();
             if (model == Cipher.DECRYPT_MODE) {
                 contentBytes = new BASE64Decoder().decodeBuffer(content);
             }
-            /**执行加密 或 解密操作。如果 contentBytes 内容过长，则 doFinal 可能会抛出异常.
-             *javax.crypto.IllegalBlockSizeException: Data must not be longer than 245 bytes ：数据不能超过xxx字节
-             * 此时需要调大 KEY_PAIR_LENGTH 的值*/
+
+            // 执行加密 或 解密操作。如果 contentBytes 内容过长，则 doFinal 可能会抛出异常.
+            // javax.crypto.IllegalBlockSizeException: Data must not be longer than 245 bytes ：数据不能超过xxx字节
+            // 此时需要调大 KEY_PAIR_LENGTH 的值
             byte[] finalBytes = cipher.doFinal(contentBytes);
+
             //如果是加密，则将加密后的字节数组使用 Base64 转成可视化的字符串。否则是解密时，直接 new String 字符串.
             if (model == Cipher.ENCRYPT_MODE) {
                 result = new BASE64Encoder().encode(finalBytes);
             } else if (model == Cipher.DECRYPT_MODE) {
                 result = new String(finalBytes);
             }
+
         } catch (Exception e) {
             e.printStackTrace();
         }
@@ -171,30 +186,38 @@ public class CipherAsymmetricEncryptionDemo {
      * @param content          明文
      * @param secureRandomSeed 安全随机数种子
      * @return 签名结果
-     * @throws NoSuchAlgorithmException
-     * @throws InvalidKeySpecException
-     * @throws InvalidKeyException
-     * @throws SignatureException
+     * @throws NoSuchAlgorithmException 无此算法异常
+     * @throws InvalidKeySpecException  无效的私钥声明异常
+     * @throws InvalidKeyException      无效的私钥异常
+     * @throws SignatureException       签名异常
      */
     private static String signatureByPrivateKey(String content, String secureRandomSeed) throws NoSuchAlgorithmException, InvalidKeySpecException, InvalidKeyException, SignatureException {
+        // 获取公私钥对
         KeyPair keyPair = generateRsaKeyPair(secureRandomSeed);
+        // 获取编码后的私钥
         byte[] privateEncoded = keyPair.getPrivate().getEncoded();
 
-        EncodedKeySpec encodedKeySpec = new PKCS8EncodedKeySpec(privateEncoded);
-        //创建 KeyFactory 对象，用于转换指定算法(RSA)的公钥/私钥。
+        //创建 KeyFactory对象，用于转换指定算法(RSA)的公钥/私钥。
         KeyFactory keyFactory = KeyFactory.getInstance("RSA");
-        //从提供的密钥规范生成私钥对象
+
+        // PKCS8编码私钥的声明，即用PKCS8编码私钥的规则
+        EncodedKeySpec encodedKeySpec = new PKCS8EncodedKeySpec(privateEncoded);
+        //从【编码的密钥声明】 生成 【私钥对象】
         PrivateKey keyPrivate = keyFactory.generatePrivate(encodedKeySpec);
 
         // 开始签名
         // 1.1 获取签名实例
         Signature signature = Signature.getInstance("sha256withrsa");
+
         // 1.2 初始化签名实例
         signature.initSign(keyPrivate);
+
         // 1.3 传入原文
         signature.update(content.getBytes());
+
         // 1.4 开始签名
         byte[] sign = signature.sign();
+
         // 1.5 使用base64编码
         return new BASE64Encoder().encode(sign);
     }
